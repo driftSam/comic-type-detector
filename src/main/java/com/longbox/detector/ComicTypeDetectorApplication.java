a38a97f7cefc7d477f5c60e723f9a3be4f05b991
pete <pjjuliana216@gmail.com>
2019-05-21 21:15:14 -0400

    Spring Project init

---
+++ src/main/java/com/longbox/detector/ComicTypeDetectorApplication.java
@@ -0,0 +1,13 @@
+package com.longbox.detector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicTypeDetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicTypeDetectorApplication.class, args);
+	}
+
+}
