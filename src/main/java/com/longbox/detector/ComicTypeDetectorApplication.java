64f54e7fd377c239c91f6f20bb49609f9aca0af8
pete <pjjuliana216@gmail.com>
2019-05-27 13:30:08 -0400

    Configured Queue, Exchange, and ListenerContainer

--- src/main/java/com/longbox/detector/ComicTypeDetectorApplication.java
+++ src/main/java/com/longbox/detector/ComicTypeDetectorApplication.java
@@ -2,11 +2,20 @@ package com.longbox.detector;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import com.longbox.detector.messaging.DetectorMessageReciever;
 import com.longbox.detector.service.ComicTypeDetectorService;
 
 @SpringBootApplication
@@ -14,6 +23,40 @@ public class ComicTypeDetectorApplication implements CommandLineRunner {
 
 	@Autowired
 	ComicTypeDetectorService service;
+
+	static final String topicExchangeName = "comic-exchange";
+
+	static final String queueName = "comic.detect";
+
+	@Bean
+	Queue queue() {
+		return new Queue(queueName, false);
+	}
+
+	@Bean
+	TopicExchange exchange() {
+		return new TopicExchange(topicExchangeName);
+	}
+
+	@Bean
+	Binding binding(Queue queue, TopicExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange).with("comic.detect.#");
+	}
+
+	@Bean
+	MessageListenerAdapter listenerAdapter(DetectorMessageReciever receiver) {
+		return new MessageListenerAdapter(receiver, "receiveMessage");
+	}
+
+	@Bean
+	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+			MessageListenerAdapter listenerAdapter) {
+		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.setQueueNames(queueName);
+		container.setMessageListener(listenerAdapter);
+		return container;
+	}
 
 	private static Logger LOG = LoggerFactory.getLogger(ComicTypeDetectorApplication.class);
 
--- src/main/java/com/longbox/detector/service/ComicTypeDetectorService.java
+++ src/main/java/com/longbox/detector/service/ComicTypeDetectorService.java
@@ -38,4 +38,5 @@ public class ComicTypeDetectorService {
 			e.printStackTrace();
 		}
 	}
+
 }
