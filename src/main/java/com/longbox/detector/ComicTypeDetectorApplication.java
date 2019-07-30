99b4700489b40e8c6e136d010a329667f9005f7d
pete <pjjuliana216@gmail.com>
2019-07-30 14:01:52 -0400

    Change dir workflow to file workflow. Consume from comic.found queue

--- src/main/java/com/longbox/detector/ComicTypeDetectorApplication.java
+++ src/main/java/com/longbox/detector/ComicTypeDetectorApplication.java
@@ -4,13 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -19,14 +18,14 @@ import com.longbox.detector.messaging.DetectorMessageReciever;
 import com.longbox.detector.service.ComicTypeDetectorService;
 
 @SpringBootApplication
-public class ComicTypeDetectorApplication implements CommandLineRunner {
+public class ComicTypeDetectorApplication {
 
 	@Autowired
 	ComicTypeDetectorService service;
 
-	static final String topicExchangeName = "comic-exchange";
+	static final String directExchangeName = "comic-exchange";
 
-	static final String queueName = "comic.detect";
+	static final String queueName = "comic.found";
 
 	@Bean
 	Queue queue() {
@@ -34,13 +33,13 @@ public class ComicTypeDetectorApplication implements CommandLineRunner {
 	}
 
 	@Bean
-	TopicExchange exchange() {
-		return new TopicExchange(topicExchangeName);
+	DirectExchange exchange() {
+		return new DirectExchange(directExchangeName);
 	}
 
 	@Bean
-	Binding binding(Queue queue, TopicExchange exchange) {
-		return BindingBuilder.bind(queue).to(exchange).with("comic.detect.#");
+	Binding binding(Queue queue, DirectExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange).with("comic.found");
 	}
 
 	@Bean
@@ -63,10 +62,9 @@ public class ComicTypeDetectorApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(ComicTypeDetectorApplication.class, args);
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		service.detect();
-	}
+	/*
+	 * @Override public void run(String... args) throws Exception {
+	 * service.detect(); }
+	 */
 
 }
